31b6e8952769d91284858959d67deb4f3948b452
Lewis Haggar <lhaggar@outlook.com>
2020-01-28 09:45:12 +0000

    added border to track padding calculation

---
+++ e2e/borders.test.ts
@@ -0,0 +1,17 @@
+import {
+  Examples,
+  getTestUrl,
+  addFontStyles
+} from './utils';
+
+jest.setTimeout(10000);
+
+beforeEach(async () => {
+  await page.goto(getTestUrl(Examples.BASIC_WITH_BORDERS));
+  await page.setViewport({ width: 600, height: 200 });
+  await addFontStyles(page);
+});
+
+test('aligns thumb as expected with borders', async () => {
+  expect(await page.screenshot()).toMatchImageSnapshot();
+});
