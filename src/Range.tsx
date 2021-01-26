f50141d4537602845e700def9ab5c3a723cac8e4
Denis <shiirochi@yandex.ru>
2021-01-26 20:12:06 +0300

    add support for empty value

--- src/Range.tsx
+++ src/Range.tsx
@@ -222,10 +222,13 @@ class Range extends React.Component<IProps> {
 
   onMouseDownTrack = (e: React.MouseEvent) => {
     if (e.button !== 0) return;
+
+    const { values, draggableTrack, direction } = this.props;
+
     e.persist();
     e.preventDefault();
     this.addMouseEvents(e.nativeEvent);
-    if (this.props.values.length > 1 && this.props.draggableTrack) {
+    if (values.length > 1 && draggableTrack) {
       if (this.thumbRefs.some(thumbRef => thumbRef.current?.contains(e.target as Node))) return;
       // handle dragging the whole track
       this.setState(
@@ -234,9 +237,14 @@ class Range extends React.Component<IProps> {
         },
         () => this.onMove(e.clientX, e.clientY)
       );
+      // case when no value has been passed
+    } else if (values.length === 0) {
+      this.setState({ draggedThumbIndex: -1 }, () => {
+        this.onMove(e.clientX, e.clientY);
+      });
     } else {
       // get the index of the thumb that is closest to the place where the track is clicked
-      const draggedThumbIndex = getClosestThumbIndex(this.thumbRefs.map(t => t.current!), e.clientX, e.clientY, this.props.direction)
+      const draggedThumbIndex = getClosestThumbIndex(this.thumbRefs.map(t => t.current!), e.clientX, e.clientY, direction)
       // move the thumb which is closest to the place where the track is clicked
       this.thumbRefs[draggedThumbIndex].current?.focus();
       this.setState(
@@ -256,7 +264,10 @@ class Range extends React.Component<IProps> {
   onTouchStartTrack = (e: React.TouchEvent) => {
     e.persist();
     this.addTouchEvents(e.nativeEvent);
-    if (this.props.values.length > 1 && this.props.draggableTrack) {
+
+    const { values, draggableTrack } = this.props;
+
+    if (values.length > 1 && draggableTrack) {
       if (this.thumbRefs.some(thumbRef => thumbRef.current?.contains(e.target as Node))) return;
       // handle dragging the whole track
       this.setState(
@@ -265,6 +276,10 @@ class Range extends React.Component<IProps> {
         },
         () => this.onMove(e.touches[0].clientX, e.touches[0].clientY)
       );
+    } else if (values.length === 0) {
+      this.setState({ draggedThumbIndex: -1 }, () => {
+        this.onMove(e.touches[0].clientX, e.touches[0].clientY);
+      });
     } else {
       // get the index of the thumb that is closest to the place where the track is clicked
       const draggedThumbIndex = getClosestThumbIndex(this.thumbRefs.map(t => t.current!), e.touches[0].clientX, e.touches[0].clientY, this.props.direction)
@@ -379,10 +394,19 @@ class Range extends React.Component<IProps> {
     );
   };
 
+  /**
+   * It moves thumb(-s) to a given clientX and clientY on track
+   */
   onMove = (clientX: number, clientY: number) => {
     const { draggedThumbIndex, draggedTrackPos } = this.state;
     const { direction, min, max, onChange, values, step, rtl } = this.props;
-    if (draggedThumbIndex === -1 && draggedTrackPos[0] === -1 && draggedTrackPos[1] === -1) return null;
+
+    // prevent this case only if values are empty,
+    // thus it will create one new value
+    if (values.length > 0) {
+      if (draggedThumbIndex === -1 && draggedTrackPos[0] === -1 && draggedTrackPos[1] === -1) return null;
+    }
+
     const trackElement = this.trackRef.current!;
     // If component was closed down prematurely, A last onMove could be triggered based on requestAnimationFrame()
     if (!trackElement) return null;
@@ -460,7 +484,18 @@ class Range extends React.Component<IProps> {
       if (rtl) {
         newValue = max + min - newValue;
       }
-      if (Math.abs(values[draggedThumbIndex] - newValue) >= step / 2) {
+
+      if (draggedThumbIndex === -1) {
+        this.thumbRefs[0] = React.createRef<HTMLElement>();
+
+        onChange(
+          replaceAt(
+            values,
+            0,
+            this.normalizeValue(newValue, 0)
+          )
+        );
+      } else if (Math.abs(values[draggedThumbIndex] - newValue) >= step / 2) {
         onChange(
           replaceAt(
             values,
