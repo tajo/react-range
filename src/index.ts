c3ab89ea5dbebaf6b4f681b4528112ff00d8b5f3
Your Name <you@example.com>
2025-02-17 13:40:30 +0300

    refactor: Add reexport types

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,28 @@ import {
   relativeValue,
   checkValuesAgainstBoundaries,
 } from "./utils";
-import { Direction } from "./types";
+import {
+  Direction,
+  type IProps as IRangeProps,
+  type IRenderMarkParams,
+  type IRenderThumbParams,
+  type IRenderTrackParams,
+  type ITrackBackground,
+  type IThumbProps,
+  type IMarkProps,
+  type ITrackProps,
+} from "./types";
 
 export {
   Range,
+  type IRangeProps,
+  type IRenderMarkParams,
+  type IRenderThumbParams,
+  type IRenderTrackParams,
+  type IThumbProps,
+  type IMarkProps,
+  type ITrackProps,
+  type ITrackBackground,
   getTrackBackground,
   Direction,
   useThumbOverlap,
